77306d862f5db3a13cdb8664906f04f6948dc683
Eagle.Lao <eagle.lao@sifive-china.com>
2020-12-11 11:29:01 +0800

    update name

--- case_bootloader/case_bootloader.py
+++ case_bootloader/case_bootloader.py
@@ -55,7 +55,7 @@ def boot_baudrate(port=None, value = None):
 	return gw_report.result(str(ret))		
 
 	
-def boot_cmdquit():
+def boot_quit():
 	ret = True
 	gw_report.clear()
 	
@@ -63,76 +63,6 @@ def boot_cmdquit():
 
 	return gw_report.result(str(ret))
 	
-
-#convert a executable hex file to flash image
-#primary: primary hex file path
-#alternate: alternate hex file path
-#alt_active: defalt is primary is active, if True, alternate is active
-#alt_addr: start address of alternate flash area
-#We agreed that the starting run address is FLASH_BASE:0x60000000 
-def boot_hexf(primary, alternate = None, alt_active = False, alt_addr = 0x100000):
-	ret = True
-	gw_report.clear()
-	
-	ih = IH()
-	bs_word = [0xffffffff]* (int(8192/4));
-	
-	#primary
-	#[[raddr,data]..]
-	prim_data = func.hexfile_analyze(primary)
-
-	bs_word[0] = len(prim_data) #IMAG_NUM
-	bs_word[2] = FLASH_BASE #IMAG_ENTRY_ADDR 
-	if(alt_active == False):
-		bs_word[3] = 0x0000_000E#IMAG_BOOT_FLG = active
-	else:
-		bs_word[3] = 0x0000_000C#IMAG_BOOT_FLG = old firmware, alternate if no active firmware valid
-		
-	#base = 0x4000
-	faddr = FLASH_BASE+0x4000
-	for i in range(len(prim_data)):
-		size = len(prim_data[i][1])
-		bs_word[int(0x100/4) + 4*i] = faddr #IMAG_ADDR flash addr
-		bs_word[int(0x104/4) + 4*i] = size#IMAG_SIZE
-		bs_word[int(0x108/4) + 4*i] = prim_data[i][0]#IMAG_RUN
-		bs_word[int(0x10c/4) + 4*i] = crc16(0,prim_data[i][1])#IMAG_CSUM
-		ih.puts(faddr, bytes(prim_data[i][1]))
-		faddr += (size + 3)&0xfffffffc # word aligned
-		
-	#alternate
-	#[[raddr,data]..]
-	if(alternate is not None):
-		alt_data = func.hexfile_analyze(alternate)
-		bs_word[0x100] = len(alt_data) #IMAG_NUM
-		bs_word[0x102] = FLASH_BASE #IMAG_ENTRY_ADDR 
-		if(alt_active == True):
-			bs_word[0x103] = 0x0000_000E#IMAG_BOOT_FLG = active
-		else:
-			bs_word[0x103] = 0x0000_000C#IMAG_BOOT_FLG = old firmware, alternate if no active firmware valid
-		
-		#base = 0x100000
-		faddr = FLASH_BASE+0x100000
-		for i in range(len(alt_data)):
-			size = len(alt_data[i][1])
-			bs_word[int(0x500/4) + 4*i] = faddr #IMAG_ADDR flash addr
-			bs_word[int(0x504/4) + 4*i] = size#IMAG_SIZE
-			bs_word[int(0x508/4) + 4*i] = alt_data[i][0]#IMAG_RUN
-			bs_word[int(0x50c/4) + 4*i] = crc16(0,alt_data[i][1])#IMAG_CSUM
-			ih.puts(faddr, bytes(alt_data[i][1]))
-			faddr += (size + 3)&0xfffffffc # word aligned
-	
-	bs_byte = [0xff]*8192
-	for i in range(0,4096,4):
-		tmp = bs_word[int(i/4)]
-		bs_byte[i] = tmp & 0xff
-		bs_byte[i+1] = (tmp>>8) & 0xff
-		bs_byte[i+2] = (tmp>>16) & 0xff
-		bs_byte[i+3] = (tmp>>24) & 0xff
-	ih.puts(FLASH_BASE + 0x2000, bytes(bs_byte))
-	
-	ih.write_hex_file(primary+'.hexf')
-	
-	return gw_report.result(str(ret))
 
 	
 #program with boot sector, idx = 0 is boot segment
@@ -317,6 +247,78 @@ def boot_write(faddr, value):
 	#print(ret)
 
 	return gw_report.result(str(ret))
+
+
+#convert a executable hex file to flash image
+#primary: primary hex file path
+#alternate: alternate hex file path
+#alt_active: defalt is primary is active, if True, alternate is active
+#alt_addr: start address of alternate flash area
+#We agreed that the starting run address is FLASH_BASE:0x60000000 
+def boot_util_hexf(primary, alternate = None, alt_active = False, alt_addr = 0x100000):
+	ret = True
+	gw_report.clear()
+	
+	ih = IH()
+	bs_word = [0xffffffff]* (int(8192/4));
+	
+	#primary
+	#[[raddr,data]..]
+	prim_data = func.hexfile_analyze(primary)
+
+	bs_word[0] = len(prim_data) #IMAG_NUM
+	bs_word[2] = FLASH_BASE #IMAG_ENTRY_ADDR 
+	if(alt_active == False):
+		bs_word[3] = 0x0000_000E#IMAG_BOOT_FLG = active
+	else:
+		bs_word[3] = 0x0000_000C#IMAG_BOOT_FLG = old firmware, alternate if no active firmware valid
+		
+	#base = 0x4000
+	faddr = FLASH_BASE+0x4000
+	for i in range(len(prim_data)):
+		size = len(prim_data[i][1])
+		bs_word[int(0x100/4) + 4*i] = faddr #IMAG_ADDR flash addr
+		bs_word[int(0x104/4) + 4*i] = size#IMAG_SIZE
+		bs_word[int(0x108/4) + 4*i] = prim_data[i][0]#IMAG_RUN
+		bs_word[int(0x10c/4) + 4*i] = crc16(0,prim_data[i][1])#IMAG_CSUM
+		ih.puts(faddr, bytes(prim_data[i][1]))
+		faddr += (size + 3)&0xfffffffc # word aligned
+		
+	#alternate
+	#[[raddr,data]..]
+	if(alternate is not None):
+		alt_data = func.hexfile_analyze(alternate)
+		bs_word[0x100] = len(alt_data) #IMAG_NUM
+		bs_word[0x102] = FLASH_BASE #IMAG_ENTRY_ADDR 
+		if(alt_active == True):
+			bs_word[0x103] = 0x0000_000E#IMAG_BOOT_FLG = active
+		else:
+			bs_word[0x103] = 0x0000_000C#IMAG_BOOT_FLG = old firmware, alternate if no active firmware valid
+		
+		#base = 0x100000
+		faddr = FLASH_BASE+0x100000
+		for i in range(len(alt_data)):
+			size = len(alt_data[i][1])
+			bs_word[int(0x500/4) + 4*i] = faddr #IMAG_ADDR flash addr
+			bs_word[int(0x504/4) + 4*i] = size#IMAG_SIZE
+			bs_word[int(0x508/4) + 4*i] = alt_data[i][0]#IMAG_RUN
+			bs_word[int(0x50c/4) + 4*i] = crc16(0,alt_data[i][1])#IMAG_CSUM
+			ih.puts(faddr, bytes(alt_data[i][1]))
+			faddr += (size + 3)&0xfffffffc # word aligned
+	
+	bs_byte = [0xff]*8192
+	for i in range(0,4096,4):
+		tmp = bs_word[int(i/4)]
+		bs_byte[i] = tmp & 0xff
+		bs_byte[i+1] = (tmp>>8) & 0xff
+		bs_byte[i+2] = (tmp>>16) & 0xff
+		bs_byte[i+3] = (tmp>>24) & 0xff
+	ih.puts(FLASH_BASE + 0x2000, bytes(bs_byte))
+	
+	ih.write_hex_file(primary+'.hexf')
+	
+	return gw_report.result(str(ret))
+
 	
 #just do xmodem transmit
 def boot_util_xmdm(path):
